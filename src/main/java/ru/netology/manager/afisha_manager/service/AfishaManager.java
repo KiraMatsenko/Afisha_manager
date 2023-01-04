9bd849a38a0c029f339a9e03b6b753b633d5f5c7
Kira <kira_pavlenko_13@mail.ru>
2023-01-04 16:44:03 +0300

    Second commit

--- src/main/java/ru/netology/manager/afisha_manager/service/AfishaManager.java
+++ src/main/java/ru/netology/manager/afisha_manager/service/AfishaManager.java
@@ -11,27 +11,30 @@ public class AfishaManager {
     public AfishaManager(int limit) {
             this.limit = limit;
     }
-    public String[] addFilm(String newFilm) { //работает не трогай
-        String[] tmp = new String[films.length + 1];
-        for (int i = 0; i < films.length; i++) {
+    public String[] addFilm(String newFilm) {
+            String[] tmp = new String[films.length + 1];
+            for (int i = 0; i < films.length; i++) {
             tmp[i] = films[i];
         }
-        tmp[tmp.length - 1] = newFilm;
-        films = tmp;
-        return films;
+            tmp[tmp.length - 1] = newFilm;
+            films = tmp;
+            return films;
     }
 
     //
     public String[] findAll() {
-        return films;
+            return films;
     }
 
     public String[] findLast() {
-        String[] reversed = new String[limit];
-        for (int i = 0; i < reversed.length; i++) {
+            if (limit > films.length) {
+                limit = films.length;
+            }
+            String[] reversed = new String[limit];
+            for (int i = 0; i < reversed.length; i++) {
             reversed[i] = films[films.length - 1 - i];
         }
-        return reversed;
+            return reversed;
     }
 }
 
--- src/test/java/AfishaManagerTest.java
+++ src/test/java/AfishaManagerTest.java
@@ -37,7 +37,7 @@ public class AfishaManagerTest {
     }
 
     @Test
-    public void shouldShowLastTenReversed() {
+    public void shouldShowExactlyLimit() {
         String filmname1 = "Бладшот";
         String filmname2 = "Вперёд";
         String filmname3 = "Отель Белград";
@@ -106,6 +106,70 @@ public class AfishaManagerTest {
                 "Вперёд",
                 "Бладшот"
                 };
+        String[] actual = service.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldShowLimitUnderAdded() {
+        String filmname1 = "Бладшот";
+        String filmname2 = "Вперёд";
+        String filmname3 = "Отель Белград";
+        String filmname4 = "Джентльмены";
+        String filmname5 = "Человек-невидимка";
+        String filmname6 = "Тролли. Мировой тур";
+        String filmname7 = "Номер один";
+        String filmname8 = "Начало";
+        String filmname9 = "Атака Титанов";
+        String filmname10 = "Симпсоны в кино";
+
+        AfishaManager service = new AfishaManager(5);
+        service.addFilm(filmname1);
+        service.addFilm(filmname2);
+        service.addFilm(filmname3);
+        service.addFilm(filmname4);
+        service.addFilm(filmname5);
+        service.addFilm(filmname6);
+        service.addFilm(filmname7);
+        service.addFilm(filmname8);
+        service.addFilm(filmname9);
+        service.addFilm(filmname10);
+
+        String[] expected = {
+                "Симпсоны в кино",
+                "Атака Титанов",
+                "Начало",
+                "Номер один",
+                "Тролли. Мировой тур"
+        };
+        String[] actual = service.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldShowLimitAboveAdded() {
+        String filmname1 = "Бладшот";
+        String filmname2 = "Вперёд";
+        String filmname3 = "Отель Белград";
+        String filmname4 = "Джентльмены";
+        String filmname5 = "Человек-невидимка";
+
+        AfishaManager service = new AfishaManager(10);
+        service.addFilm(filmname1);
+        service.addFilm(filmname2);
+        service.addFilm(filmname3);
+        service.addFilm(filmname4);
+        service.addFilm(filmname5);
+
+        String[] expected = {
+                "Человек-невидимка",
+                "Джентльмены",
+                "Отель Белград",
+                "Вперёд",
+                "Бладшот"
+        };
         String[] actual = service.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
